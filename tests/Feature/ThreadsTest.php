0ee85bd84684443889a32860fd027874a2fc9651
ADETUNJI OPAYELE <lordwhizy@gmail.com>
2017-04-21 17:22:53 +0100

    TDD Threads Basics

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -3,23 +3,42 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ThreadsTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function a_user_can_browse_threads()
+
+    /** @test */
+    public function a_user_can_view_all_threads()
     {
+        $thread = factory('App\Thread')->create();
+
         $response = $this->get('/threads');
 
         $response->assertStatus(200);
 
-        
+        $response->assertSee($thread->title);
+
+        $response = $this->get('/threads/' . $thread->id);
+
+        $response->assertSee($thread->title);
+    }
+
+        /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+
+        $response->assertStatus(200);
+
+        $response->assertSee($thread->title);
     }
 }
